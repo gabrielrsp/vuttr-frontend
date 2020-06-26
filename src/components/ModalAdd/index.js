237e7fa5e9d19a00b59d089e2078d57d8343e018
gabrielrsp <gabrielrsp18@gmail.com>
2020-06-26 01:07:51 -0300

    adjust workflow

--- src/components/ModalAdd/index.js
+++ src/components/ModalAdd/index.js
@@ -33,7 +33,7 @@ export default function ModalAdd({ onAddModal, onAddTool, onChangeTitle, onChang
         <h2>Add New Tool</h2>
       </div>
 
-      <h4 className="title" placeholder="" > DEPLOY</h4>
+      <h4 className="title" placeholder="" >TESTE DEPLOY</h4>
       <input type="text" placeholder="Tool title" onChange={onChangeTitle} />
 
       <h4 className="title" >Tool Link</h4>
