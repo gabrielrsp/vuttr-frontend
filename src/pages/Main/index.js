5fbeea6785c0c1ede843185b59dc5274989a7759
gabrielrsp <gabrielrsp18@gmail.com>
2020-06-28 20:58:42 -0300

    change toast message on edit modal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -132,7 +132,7 @@ export default function Main() {
       setNewTags('');
       setIdClick('')
       closeEditModal()
-      toast.success('Tool Updated');
+      toast.success('Tool is Updated');
     }
 
   }
